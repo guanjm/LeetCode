fff36a8d0effeb078ff555dfd3a44b5a2bc76660
Tim <a511190813@126.com>
2020-11-13 00:14:21 +0800

    优化LeetCode_2

--- LeetCode/src/indi/gjm/no_2/AddTwoNumbers.java
+++ LeetCode/src/indi/gjm/no_2/AddTwoNumbers.java
@@ -9,7 +9,7 @@ package indi.gjm.no_2;
 public class AddTwoNumbers {
 
     private static ListNode getResult(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode();
+        ListNode result = null;
         ListNode lastNode = null;
         int add = 0;
         while (l1 != null || l2 != null || add > 0) {
@@ -24,41 +24,73 @@ public class AddTwoNumbers {
                 l2 = l2.getNext();
             }
             int val3 = val1 + val2 + add;
-            if (val3 > 9) {
+            if (val3 > 10) {
                 add = 1;
-                val3 = val3 - 9;
+                val3 = val3 - 10;
             } else {
                 add = 0;
             }
-            ListNode nextNode;
+            ListNode currentNode;
             if (lastNode == null) {
-                nextNode = result;
+                result = lastNode = new ListNode();
+                lastNode.setVal(val3);
             } else {
-                nextNode = new ListNode();
-                lastNode.setNext(nextNode);
+                currentNode = new ListNode();
+                lastNode.setNext(currentNode);
+                currentNode.setVal(val3);
+                lastNode = currentNode;
             }
-            nextNode.setVal(val3);
-            lastNode = nextNode;
         }
         return result;
     }
 
     public static void main(String[] args) {
         //4 -> 3 -> 2 -> 1 (1234)
-        ListNode l14 = new ListNode(4);
-        ListNode l13 = new ListNode(3, l14);
-        ListNode l12 = new ListNode(2, l13);
-        ListNode l11 = new ListNode(1, l12);
+        ListNode l11 = generateListNode1("1234");
+        System.out.println(getString(l11));
         //8 -> 7 -> 6 -> 5 (5678)
-        ListNode l24 = new ListNode(8);
-        ListNode l23 = new ListNode(7, l24);
-        ListNode l22 = new ListNode(6, l23);
-        ListNode l21 = new ListNode(5, l22);
-
+        ListNode l21 = generateListNode1("5678");
+        System.out.println(getString(l21));
+        //2 -> 1 -> 9 -> 6 (6912)
         ListNode result = getResult(l11, l21);
-        while (result != null) {
-            System.out.println(result.getVal());
-            result = result.getNext();
+        System.out.println(getString(result));
+    }
+
+    //生成链表（循环）
+    private static ListNode generateListNode1(String str) {
+        ListNode result = null;
+        ListNode last = null;
+        for (int i = str.length()-1; i >= 0; i--) {
+            if (last == null) {
+                result = last = new ListNode();
+                last.setVal(str.charAt(i) - '0');
+            } else {
+                ListNode currentNode = new ListNode();
+                last.setNext(currentNode);
+                currentNode.setVal(str.charAt(i) - '0');
+                last = currentNode;
+            }
+        }
+        return result;
+    }
+
+    //生成链表（递归）
+    private static ListNode generateListNode2(String str) {
+        if (str == null || str.length() < 1) {
+            return null;
+        } else {
+            ListNode listNode = new ListNode();
+            listNode.setVal(str.charAt(str.length() - 1) - '0');
+            listNode.setNext(generateListNode2(str.substring(0, str.length() - 1)));
+            return listNode;
+        }
+    }
+
+    private static String getString(ListNode listNode) {
+        if (listNode != null) {
+            return getString(listNode.getNext()) + listNode.getVal();
+        } else {
+            return "";
         }
     }
 
