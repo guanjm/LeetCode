7737d52410506e3a66f37db9a15eb1bea5e81668
ex_guanjm <511190813@qq.com>
2020-11-13 17:56:36 +0800

    LeetCode_5

---
+++ LeetCode/src/indi/gjm/no_5/LongestPalindrome.java
@@ -0,0 +1,21 @@
+package indi.gjm.no_5;
+
+/**
+ * 最长回文子串
+ * @Author : guanjm
+ * @Date: 2020/11/13 15:32
+ *
+ */
+public class LongestPalindrome {
+
+    private static String getResult(String s) {
+        String result = "";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbbd";
+        System.out.println(getResult(s));
+    }
+
+}
