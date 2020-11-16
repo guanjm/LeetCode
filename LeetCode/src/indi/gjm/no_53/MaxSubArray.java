a84d9151af77bd87adfa81e599e060a3fed17322
ex_guanjm <511190813@qq.com>
2020-11-16 18:32:25 +0800

    LeetCode_53

---
+++ LeetCode/src/indi/gjm/no_53/MaxSubArray.java
@@ -0,0 +1,21 @@
+package indi.gjm.no_53;
+
+/**
+ * 最大子序和
+ * @Author : guanjm
+ * @Date: 2020/11/16 17:49
+ *
+ */
+public class MaxSubArray {
+
+    private static int getResult(int[] nums) {
+        
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(getResult(nums));
+    }
+
+}
