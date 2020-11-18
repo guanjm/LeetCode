af14c3f1ed556a2505a8712de8147e00557941bf
ex_guanjm <511190813@qq.com>
2020-11-18 13:48:36 +0800

    LeetCode_1030

--- LeetCode/src/indi/gjm/no_1030/AllCellsDistOrder.java
+++ LeetCode/src/indi/gjm/no_1030/AllCellsDistOrder.java
@@ -1,7 +1,9 @@
 package indi.gjm.no_1030;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * 距离顺序排序矩阵单元格
@@ -11,7 +13,7 @@ import java.util.Comparator;
  */
 public class AllCellsDistOrder {
 
-    private static int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+    private static int[][] allCellsDistOrder1(int R, int C, int r0, int c0) {
         int[][] result = new int[R*C][2];
         int count = 0;
         for (int i = 0; i < R; i++) {
@@ -23,10 +25,37 @@ public class AllCellsDistOrder {
         return result;
     }
 
+    private static int[][] allCellsDistOrder2(int R, int C, int r0, int c0) {
+        int[][] result = new int[R*C][];
+        int count = 0;
+        int maxDistance = Math.max(R - r0, r0) + Math.max(C - c0, c0);
+        int distance = 0;
+        while (maxDistance >= distance) {
+            for (int i = -distance; i <= distance; i++) {
+                int r = r0 + i;
+                int tmp = distance - Math.abs(r - r0);
+                int c1 = c0 + tmp;
+                int c2 = c0 - tmp;
+                if (tmp == Math.abs(c0 - c1)) {
+                    if (0 <= r && r < R && 0 <= c1 && c1 < C) {
+                        result[count++] = new int[]{r, c1};
+                    }
+                }
+                if (tmp == Math.abs(c0 - c2) && c1 != c2) {
+                    if (0 <= r && r < R && 0 <= c2 && c2 < C) {
+                        result[count++] = new int[]{r, c2};
+                    }
+                }
+            }
+            distance++;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
-        int[][] cells = allCellsDistOrder(2, 3, 1, 2);
+        int[][] cells = allCellsDistOrder2(2, 3, 1, 2);
         for (int[] cell : cells) {
-            System.out.println("["+cell[0]+", "+ cell[1]+"])");
+            System.out.println("["+cell[0]+", "+ cell[1]+"]");
         }
     }
 
