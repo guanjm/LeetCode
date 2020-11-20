736287bd771dec07c3d4be73e4c2a1590e3a5f9c
ex_guanjm <511190813@qq.com>
2020-11-20 18:32:12 +0800

    LeetCode_147

---
+++ LeetCode/src/indi/gjm/no_147/InsertionSortList.java
@@ -0,0 +1,21 @@
+package indi.gjm.no_147;
+
+import indi.gjm.po.ListNode;
+
+/**
+ * 对链表进行插入排序
+ * @Author : ex_guanjm
+ * @Date: 2020/11/20 14:15
+ *
+ */
+public class InsertionSortList {
+
+    public ListNode getResult(ListNode head) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ LeetCode/src/indi/gjm/po/ListNode.java
@@ -0,0 +1,11 @@
+package indi.gjm.po;
+
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) { val = x; }
+
+}
