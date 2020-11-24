7b4eaa08b0290a4acfd45e74a3ff5af595d23727
Tim <a511190813@126.com>
2020-11-24 08:22:50 +0800

    leetcode_452

---
+++ LeetCode/src/indi/gjm/no_452/FindMinArrowShots.java
@@ -0,0 +1,21 @@
+package indi.gjm.no_452;
+
+/**
+ * 用最少数量的箭引爆气球
+ * @author : guanjm
+ * @Date: 2020/11/23
+ *
+ */
+public class FindMinArrowShots {
+
+    private static int getResult(int[][] points) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = new int[][]{{10, 16}, {2, 8}, {1, 6}, {7, 12}};
+        System.out.println(getResult(points));
+    }
+
+}
